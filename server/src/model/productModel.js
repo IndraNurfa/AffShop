2bd1cf941388c0144039311d0df0689b7b015e90
Ahmad Indra Nurfauzi <ahmadindran@gmail.com>
2023-08-18 09:33:47 +0700

    ull-app affshop

---
+++ server/src/model/productModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    videoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Video',
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }, image: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
